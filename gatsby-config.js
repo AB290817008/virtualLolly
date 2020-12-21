986d2434c280bde6703a8d7b8794e5eab0caa4a6
AB290817008 <42501497+AB290817008@users.noreply.github.com>
2020-12-21 22:49:37 +0500

    Update1

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,25 +4,25 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-// module.exports = {
-//   /* Your site config here */
-//   plugins: [],
-// }
-
 module.exports = {
-  plugins: [
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // This type will contain remote schema Query type
-        typeName: "",
-        // This is the field under which it's accessible
-        fieldName: "",
-        // URL to query from
-        url: "http://localhost:8888/.netlify/functions/newLolly",
-      },
-    },
+  /* Your site config here */
+  plugins: [],
+}
+
+// module.exports = {
+//   plugins: [
+//     {
+//       resolve: "gatsby-source-graphql",
+//       options: {
+//         // This type will contain remote schema Query type
+//         typeName: "",
+//         // This is the field under which it's accessible
+//         fieldName: "",
+//         // URL to query from
+//         url: "http://localhost:8888/.netlify/functions/newLolly",
+//       },
+//     },
 
     
-  ],
-}
+//   ],
+// }
